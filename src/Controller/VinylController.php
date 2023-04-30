7e6038cbd050a323a7c29b550df1118073ffad70
Anton Savelyev <tensor@antons-mbp.kpn>
2023-04-30 13:27:50 +0200

    route -> controller -> response -> profit

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;  //symfony is a set of libraries that gives us tons of tools
+                                                //for logging, making database queries, sending emails, rendering templates, making API calls etc.
+                                                //~100 separate libraries
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+class VinylController //controller or action
+{
+    #[Route('/')]   //php attribute
+                    // linked to controller below
+                    // points to a link
+    public function homepage(): Response //controller return type
+    {
+        return new Response('Title: Pb and Jams'); //controllers must return Response objects (might be worked around)
+    }
+    #[Route('/browse/{slug}')]  //wild card {wildcard}
+                                //each wild card is matched to a controller by name not by position
+                                //can be anything
+                                //when inserted we are allowed to have a function attribute of a same name $slug
+                                //if we go to /browse/death-metal it will match $slug route and pass this string to the {slug} argument
+                                //matching is done by name
+    public function browse(string $slug = null): Response   //null make /browse page work
+                                                            //you can have argument with name $slug but is not required
+    {
+        if($slug)
+        {
+            $title = 'Genre: ' .u(str_replace('-', ' ', $slug))->title(true); //u() is a symfony function
+            //from symfony/string library/component
+        }
+        else {
+            $title = 'All genres';
+        }
+
+        return new Response($title);
+    }
+}
